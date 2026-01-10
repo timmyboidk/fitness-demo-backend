6c2c96f508612a0820368b260b6b5b86a5816204
timmyboidk <zaiyangyu369@gmail.com>
2026-01-09 19:04:41 -0500

    Initial commit

---
+++ src/main/java/com/example/fitness_demo_backend/FitnessDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitness_demo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessDemoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fitness_demo_backend/FitnessDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitness_demo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessDemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
