47b525058600a3b377d5b1d54ada899b0bd42d54
timmyboidk <zaiyangyu369@gmail.com>
2026-01-09 22:33:53 -0500

    GlobalExceptionHandler 编译修复

--- fitness-common/src/main/java/com/example/fitness/common/exception/GlobalExceptionHandler.java
+++ fitness-common/src/main/java/com/example/fitness/common/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.example.fitness.common.exception;
 
+import com.example.fitness.common.result.ErrorCode;
 import com.example.fitness.common.result.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
